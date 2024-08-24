8226fbbacdd9b81b9864aef284ffbe1fed34165a
Juan Valero <juanvalero@cosmo.edu.co>
2024-08-24 09:36:32 -0500

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+    return (
+        <div className="text-3xl font-bold">
+            <h1>My App</h1>
+            <p>Welcome to my app!</p>
+        </div>
+    );
+}
