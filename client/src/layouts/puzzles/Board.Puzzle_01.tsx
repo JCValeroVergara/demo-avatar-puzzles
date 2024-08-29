2153e93f4e0e9abaa03c320162f9358348233ad4
Juan Valero <juanvalero@cosmo.edu.co>
2024-08-28 23:09:09 -0500

    feat: Add showWinner state and handleCloseClick function
    This commit adds the  state and  function to the  component. The  state is used to control the visibility of the  component, which displays a congratulatory message when the puzzle is solved. The  function is triggered when the user closes the  component. These additions enhance the user experience by providing feedback and interaction after solving the puzzle.
    
    Note: This commit message follows the conventional commit format, starting with a type (feat) followed by a brief description of the changes.

--- client/src/layouts/puzzles/Board.Puzzle_01.tsx
+++ client/src/layouts/puzzles/Board.Puzzle_01.tsx
@@ -1,10 +1,11 @@
 import { FC, useState } from "react";
 import { getImages } from "../../common/puzzles/imagen.utils";
-import { ButtonAssistant, DialogBoxPuzzleInstructions, Frame_Puzzles_Guau } from "../../components";
+import { ButtonAssistant, DialogBoxPuzzleInstructions, Frame_Puzzles_Guau, PuzzleWinner } from "../../components";
 
 type GridType = (number | string)[][];
 
 export const Board_Puzzle_01: FC = () => {
+    const [showWinner, setShowWinner] = useState(false);
 
     const [grid, setGrid] = useState<GridType>([
         [1, 2, 3, 4],
@@ -72,54 +73,67 @@ export const Board_Puzzle_01: FC = () => {
 
             setTimeout(() => {
                 if (isSolved(grid)) {
-                    alert("¡Felicidades! Has resuelto el puzzle");
+                    setShowWinner(true);
                 }
             }, 100);
         }
     };
 
+    const handleCloseClick = () => {
+        setShowWinner(false);
+    };
+
 
     
     return (
-        <div className="w-full h-full flex flex-col items-start">
-            <div className="flex flex-row items-center justify-center w-full h-1/6 border bg-slate-500 bg-opacity-90 text-slate-50 text-[5vh] font-extrabold">
-            <h1>Desafío Claustro de la Imaginación</h1>
-            </div>
-            <div className="w-[69vw] h-5/6 flex justify-center items-center">
-            <div className="w-[30vw] h-full flex flex-col justify-center items-center">
-                <div className="w-full h-1/3 flex items-end">
+        <>
+        {showWinner && (
+            <PuzzleWinner
+                onClose={handleCloseClick}
+                textBox="¡Acertaste! Has resuelto el puzzle."
+                textWin="Puedes jugar este Desafío cuando quieras, pero solo recibirás la recompensa al acertar una vez semanalmente."
+            />
+        )}
+            <div className="w-full h-full flex flex-col items-start">
+                <div className="flex flex-row items-center justify-center w-full h-1/6 border bg-slate-500 bg-opacity-90 text-slate-50 text-[5vh] font-extrabold">
+                <h1>Desafío Claustro de la Imaginación</h1>
+                </div>
+                <div className="w-[69vw] h-5/6 flex justify-center items-center">
+                <div className="w-[30vw] h-full flex flex-col justify-center items-center">
+                    <div className="w-full h-1/3 flex items-end">
                     <DialogBoxPuzzleInstructions text="Toca las piezas que están alrededor de la pieza vacia hasta completa la imagen" />
-                </div>
-                <div className="w-full h-1/2 ">
-                    <Frame_Puzzles_Guau />
-                </div>
-            </div>
-            <div className="w-[30vw] h-[30vw] grid grid-cols-4 border-[0.5vw] border-tablerobg">
-                {grid.map((row, rowIndex) =>
-                row.map((cell, collIndex) => (
-                    <div
-                    key={`${rowIndex}-${collIndex}`}
-                    className={`w-full h-full border bg-gray-300 flex items-center justify-center text-xl font-bold cursor-pointer ${
-                        cell === '' ? ' bg-slate-500 opacity-90' : ''
-                        }`}
-                    onClick={() => moveCell(rowIndex, collIndex)}
-                    >
-                    {cell !== '' && (
-                        <img src={getImages(cell as number)} alt={`Imagen ${cell}`} />
-                    )}
                     </div>
-                ))
-                )}
-            </div>
-            </div>
-            <div className="w-full h-1/6 flex justify-center items-center">
+                    <div className="w-full h-1/2 ">
+                    <Frame_Puzzles_Guau />
+                    </div>
+                </div>
+                <div className="w-[30vw] h-[30vw] grid grid-cols-4 border-[0.5vw] border-tablerobg">
+                    {grid.map((row, rowIndex) =>
+                    row.map((cell, collIndex) => (
+                        <div
+                        key={`${rowIndex}-${collIndex}`}
+                        className={`w-full h-full border bg-gray-300 flex items-center justify-center text-xl font-bold cursor-pointer ${
+                            cell === "" ? " bg-slate-500 opacity-90" : ""
+                        }`}
+                        onClick={() => moveCell(rowIndex, collIndex)}
+                        >
+                        {cell !== "" && (
+                            <img
+                            src={getImages(cell as number)}
+                            alt={`Imagen ${cell}`}
+                            />
+                        )}
+                        </div>
+                    ))
+                    )}
+                </div>
+                </div>
+                <div className="w-full h-1/6 flex justify-center items-center">
                 <div className="w-1/4 h-[8vh]">
-                    <ButtonAssistant
-                        onClick={shuffle}
-                        text="Barajar"
-                    />
+                    <ButtonAssistant onClick={shuffle} text="Barajar" />
+                </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
